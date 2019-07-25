1309855432f858eb8488de783720c785856f59e8
unknown <hassangillani1994@gmail.com>
2019-07-25 11:26:35 -0700

    Default Open is Added

--- src/components/accordion/index.js
+++ src/components/accordion/index.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+// import AccordionBodyHide from '../../components/common/accordion-body-hide/';
+// import AccordionBodyShow from '../../components/common/accordion-body-show/';
+
 
 import './styles.css';
 
@@ -6,26 +9,28 @@ class Accordion extends Component {
     constructor(props){
         super(props)
         this.state = {
-            isOpen: false
+            isOpen: this.props.isOpen
         }
     }
-    clickHandler= (e) =>{
-        e.preventDefault();
+    clickHandler= () =>{
+        
         this.setState({
-            isOpen: !this.state.isOpen,
+            isOpen: !this.state.isOpen
             
         })
-        
-        
     }
 render() {
-    const { title, icon, children } = this.props;
-    const {isOpen} = this.state;
+    const { title, icon, children} = this.props;
+    const {isOpen}=this.state; 
     return <>
        
-            <li className={`${isOpen ? 'active' : ''}`}>
-                <div className="accordion-header" onClick={(e) => this.clickHandler(e)}><i className="material-icons">{icon}</i>{title}</div>
-                <div className={`accordion-body ${isOpen ? 'open' : ''}`}  ><span>{children}</span></div>
+            <li>
+                <div className="accordion-header"  onClick={this.clickHandler} >
+                    <i className="material-icons">{icon}</i>{title}
+                </div >
+                <div className={`accordion-body ${isOpen ? 'open' : ''}`}>
+                     <span>{children}</span>
+                </div>
             </li>
         
     
--- src/view/app/index.js
+++ src/view/app/index.js
@@ -11,6 +11,7 @@ import 'materialize-css/dist/css/materialize.min.css';
 import Accordion from '../../components/accordion/';
 
 class App extends Component {
+  
  render() {
      return <>
             <nav>
@@ -20,9 +21,11 @@ class App extends Component {
             </nav>
             <div className='container mt-4'>
             <ul className='collapsible'>
-                <Accordion title='Home' icon='filter_drama'>
-                    
-Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean metus eros, imperdiet tempus dictum sed, finibus sed eros. Vivamus luctus gravida condimentum. Quisque auctor risus id quam bibendum ultrices. In et augue urna. Curabitur efficitur risus nec nisl sagittis, sit amet placerat erat fermentum. Aenean ut posuere sapien. Donec facilisis malesuada augue, sed tempus orci eleifend eu. Duis nec elementum turpis. Phasellus ut libero diam. Pellentesque odio sapien, sodales a commodo ac, mattis cursus felis. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc eget mi vel nisi pulvinar tristique congue vel nisi. Proin vitae ligula neque. Nulla cursus sodales.
+                <Accordion title='Home' icon='filter_drama' isOpen='true' >                
+                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                   Aenean metus eros, imperdiet tempus dictum sed, finibus sed eros. 
+                   Vivamus luctus gravida condimentum. Quisque auctor risus id quam bibendum ultrices. 
+                   In et augue urna. Curabitur efficitur risus nec nisl sagittis ursus sodales.
                 </Accordion>
                 <Accordion title='About Us' icon='whatshot'>
                     Lorem ipsum dolor sit amet 
